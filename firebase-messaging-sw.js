88c228b0521c23aef578e69881f1ecc2f7b21986
kasinojp1 <v42058606@gmail.com>
2025-11-13 14:07:00 +0700

    firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,21 @@
+importScripts('https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/10.1.0/firebase-messaging.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvqbWpS1s_l72Fh8CLxO2jWFnEbSSIGSk",
+  authDomain: "kasinojpwork.firebaseapp.com",
+  projectId: "kasinojpwork",
+  storageBucket: "kasinojpwork.firebasestorage.app",
+  messagingSenderId: "291449017470",
+  appId: "1:291449017470:web:14d6815edcd61f3618b42f",
+  measurementId: "G-D648ETD7YD"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload){
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = { body: payload.notification.body };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
