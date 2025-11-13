2e7eb7305e4b7bb71dad41fd76ea25c883fb74c1
kasinojp1 <v42058606@gmail.com>
2025-11-13 17:07:33 +0700

    firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,24 @@
-importScripts('https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/10.1.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.0/firebase-messaging.js');
 
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyCvqbWpS1s_l72Fh8CLxO2jWFnEbSSIGSk",
   authDomain: "kasinojpwork.firebaseapp.com",
+  databaseURL: "https://kasinojpwork-default-rtdb.firebaseio.com",
   projectId: "kasinojpwork",
   storageBucket: "kasinojpwork.firebasestorage.app",
   messagingSenderId: "291449017470",
   appId: "1:291449017470:web:14d6815edcd61f3618b42f",
   measurementId: "G-D648ETD7YD"
-};
+});
 
-firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// Background notifications
-messaging.onBackgroundMessage(function(payload) {
-  const title = payload.notification?.title || "KasinoJP";
-  const options = { body: payload.notification?.body || "", icon: "/favicon.ico" };
-  self.registration.showNotification(title, options);
+messaging.onBackgroundMessage((payload) => {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
